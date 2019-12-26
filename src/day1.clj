e6c81f720fdd38c3fe12ccdef1fcc0fc2d1c359d
Adrian Smith <sfyire@gmail.com>
2019-12-26 17:42:17 +0000

    Part 2

--- src/day1.clj
+++ src/day1.clj
@@ -9,6 +9,16 @@
 (defn mass->fuel [mass]
   (- (Math/floor (/ mass 3)) 2))
 
-(apply +
-  (map mass->fuel (payload-mass)))
-;; 3367126
+(def fuel-requirement
+  (apply +
+    (map mass->fuel (payload-mass)))) ;; 3367126
+
+(defn sum-tail-fuel [mass]
+  (apply +
+    (take-while pos?
+      (iterate mass->fuel mass))))
+
+(def improved-fuel-requirement
+  (->> (map mass->fuel (payload-mass))
+       (map sum-tail-fuel)
+       (apply +))) ;; 5047796
